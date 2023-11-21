2722aa237752c230a447cbd70cbfbd005e070bdd
Danil Kuzychenko <lymaaaan@gmail.com>
2023-11-21 22:35:22 +0400

    Feat: PatientCard was added

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,26 @@
+﻿using Hospital.Models.Infrastucture;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class ClientController : Controller
+    {
+        [HttpGet]
+        public IActionResult PatientRegistration() => View();
+
+        [HttpPost]
+        public IActionResult PatientRegistration(Patient patient)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PatientCard() => View();
+
+        [HttpPost]
+        public IActionResult LoadPatientCard()
+        {
+            return RedirectToAction("PatientCard");
+        }
+    }
+}
---
+++ Views/Client/PatientCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hospital.Views.Client
+{
+    public class PatientCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
