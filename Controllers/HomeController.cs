c55276c466a1f95d020ed3fabffc74c24770eb01
Danil Kuzychenko <lymaaaan@gmail.com>
2023-11-22 15:56:29 +0400

    Feat: Registration/Authorization were added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hospital.Models.Infrastucture;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hospital.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MainContext _context;
+        public HomeController(MainContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index() => View();
 
         [HttpGet]
@@ -14,9 +22,12 @@ namespace Hospital.Controllers
         {
             if (login != null && password != null)
             {
+                User u = new() { Login = login, Password = password, RoleId = 1};
+                _context.Users.Add(u);
+                _context.SaveChanges();
+
                 return RedirectToAction("Index");
             }
-
             return View();
         }
 
@@ -28,7 +39,12 @@ namespace Hospital.Controllers
         {
             if(login != null && password != null)
             {
-                return RedirectToAction("Index");
+                User u = _context.Users.FirstOrDefault(u => u.Login == login && u.Password == password);
+
+                if (u != null)
+                {
+                    return RedirectToAction("Index");
+                }
             }
             return View();
         }
--- Models/Infrastucture/MainContext.cs
+++ Models/Infrastucture/MainContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hospital.Models.Infrastucture
 {
